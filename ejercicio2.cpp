1e53628b684597eb7036656678d35bcb60dd50d4
Nico Rodriguez <nicorodriguezsosa@gmail.com>
2024-09-11 22:42:30 -0300

    Ej2 <Constructor, estructura y funcion insertar>

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -5,6 +5,54 @@
 
 using namespace std;
 
+ struct Libro
+{
+    int id;
+    string titulo;
+    bool disp;
+    bool borrado;
+
+    Libro(int id, string titulo) : id(id), titulo(titulo), disp(true), borrado(false) {}
+};
+
+class HASH
+{
+    private:
+        Libro** vector;
+        int buckets;
+        int cantElem;
+
+        void rehash(){
+            buckets = sigPrimo(buckets);
+
+
+        }
+        
+        int fnHash1(){
+            
+        }
+
+        void insertarAux(int id, string titulo){
+            
+        }
+        
+
+    public:
+
+        HASH(int tamanio){
+            this->buckets = tamanio;
+            this->cantElem = 0;
+            this->vector = new Libro* [this->buckets] ();
+        }
+
+        void insertar (int id, string titulo){
+            if(((float)(cantElem + 1)/buckets) > 0.7){
+                this->rehash();
+            } else this->insertarAux(id,titulo);
+        }
+
+};
+
 int main()
 {
     // TODO
